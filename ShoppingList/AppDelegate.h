a36599547f8108cf56749dd17b59ce9887493378
Marek <marek.krzysztof.helak@gmail.com>
2015-04-20 19:11:44 +0200

    Initial Commit

---
+++ ShoppingList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingList
+//
+//  Created by Marek on 20.04.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingList
+//
+//  Created by Marek on 20.04.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
